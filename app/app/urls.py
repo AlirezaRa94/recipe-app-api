5f0f7e374ce1567d901a6d850db0295fb30296e4
Alireza Raei <alirezaraei17@gmail.com>
2023-07-09 20:41:39 +1000

    Add SpectacularAPIView and SpectacularSwaggerView to urlpatterns
    In this commit, we are adding two new paths to the urlpatterns of Django. The first path is 'api/schema/' which is mapped to the SpectacularAPIView class using the as_view() method. The as_view() method helps in converting the class-based view into a function-based view compatible with Django's URL routing system. The name parameter is set to 'api-schema', which provides a unique identifier for this URL pattern.
    
    The second path is 'api/docs/' which is mapped to the SpectacularSwaggerView class using the as_view() method. The url_name parameter is set to 'api-schema', which specifies the name of the URL pattern to be used when generating the URL for the SpectacularSwaggerView. This ensures that the SpectacularSwaggerView depends on the 'api-schema' URL pattern.
    
    Commit details:
    
    - Added path 'api/schema/' mapped to SpectacularAPIView.as_view() with the name 'api-schema'. This provides the endpoint for accessing the Spectacular API schema.
    - Added path 'api/docs/' mapped to SpectacularSwaggerView.as_view(url_name='api-schema') with the name 'api-docs'. This sets up the documentation endpoint for the Spectacular API using the Swagger UI, which depends on the 'api-schema' URL pattern.

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -22,4 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
