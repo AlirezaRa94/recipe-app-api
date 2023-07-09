1c54a58793e3b3f8e3d42477bb8d34d394dca211
Alireza Raei <alirezaraei17@gmail.com>
2023-07-09 20:22:59 +1000

    Add the drf spectacular configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'user',
     'recipe',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
