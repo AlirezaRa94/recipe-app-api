db7f508177a79ba6793e2e4593638dc0a2443a21
Alireza Raei <alirezaraei17@gmail.com>
2023-07-28 22:37:13 +1000

    Fix the linting problem in admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,16 +15,14 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
-        (_('Permissions'),
-            {
-                'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser'
-                )
-            }
-        ),
-        (_('Important Dates'), {'fields': ('last_login', )})
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+        (_('Important Dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
